419d59463e1f197756de2b57f07478c1135335b3
GuzeevZhenya <shyst.evgeny@mail.ru>
2023-07-14 10:47:59 +0300

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './index.css';
-import AppWithRedux from './AppWithRedux';
-import {createRoot} from 'react-dom/client';
-import * as serviceWorker from './serviceWorker';
-import {store} from './state/store';
-import {Provider} from 'react-redux';
+import React from "react";
+import "./index.css";
+import AppWithRedux from "./AppWithRedux";
+import { createRoot } from "react-dom/client";
+import * as serviceWorker from "./serviceWorker";
+import { store } from "./state/store";
+import { Provider } from "react-redux";
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
